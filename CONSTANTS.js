ae508dfc38c570961753bf8bea923977bfb2eb55
swenlii <swenliw@gmail.com>
2022-06-13 19:05:00 +0300

    add constants

---
+++ CONSTANTS.js
@@ -0,0 +1,12 @@
+let db_host = 'containers-us-west-65.railway.app'
+let db_user = 'postgres'
+let db_pass = 'tMnLDKj1g64P1n54PSwo'
+let db_name = 'railway'
+let db_port = '6713'
+
+let mail_host = 'smtp.gmail.com'
+let mail_port = '465'
+let mail_user = 'swenliw@gmail.com'
+let mail_pass = 'rcwujrfccqmgpvvo'
+
+export {db_host, db_user, db_pass, db_name, db_port, mail_host, mail_port, mail_user, mail_pass}
